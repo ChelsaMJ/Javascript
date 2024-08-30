dfba07911d6e0d07cbab79ba9cce10693aa334be
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-30 22:57:14 +0530

    Create Hello World.js

---
+++ Hello World.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+
+const f = createHelloWorld();
+console.log(f()); // "Hello World"
+console.log(f({}, null, 42)); // "Hello World"
